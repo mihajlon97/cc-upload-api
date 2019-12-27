9c0a683809238db8815b1674736c81f552f6b4cf
Mihajlo Nikodijevic <a01646292@unet.univie.ac.at>
2019-12-28 00:22:49 +0100

    - added lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,29 +1,36 @@
 console.log('Loading function');
 
 const aws = require('aws-sdk');
-// const request = require('request');
+const axios = require('axios');
 
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 exports.handler = async (event, context) => {
-    console.log(event);
-    // Get the object from the event and show its content type
-    const params = {
-        Bucket: 'blurring-images',
-        Prefix: 'blurred-1Lq68k2'
-    };
-    try {
-        const { Contents } = await s3.listObjectsV2(params).promise();
-        console.log('CONTENTS: ', Contents.length);
-        if (Contents.length === 4) {
-            // const res = await request.post('https://dcd3ece4.ngrok.io/complete/1Lq68k2', {form:{}})
-        }
-        return Contents;
-    } catch (err) {
-        console.log(err);
-        const message = `Error getting objects from bucket. Make sure they exist and your bucket is in the same region as this function.`;
-        console.log(message);
-        throw new Error(message);
-    }
+	const blurringId = event.Records[0].s3.object.key.substring(
+		event.Records[0].s3.object.key.lastIndexOf("d-") + 2,
+		event.Records[0].s3.object.key.lastIndexOf("/")
+	);
+	// Get the object from the event and show its content type
+	const params = {
+		Bucket: 'blurring-images',
+		Prefix: 'blurred-' + blurringId
+	};
+	try {
+		const { Contents } = await s3.listObjectsV2(params).promise();
+		if (Contents.length === 4) {
+			const res = await axios.post('http://ec2-18-184-231-193.eu-central-1.compute.amazonaws.com:7878/complete/' + blurringId, {event})
+			/*
+			// Ngrok tunneling used for development on local machine :)
+			// const res = await axios.post('https://dcd3ece4.ngrok.io/complete/' + blurringId, {event})
+			 	console.log(res.data);
+			*/
+		}
+		return Contents;
+	} catch (err) {
+		console.log(err);
+		const message = `Error getting objects from bucket. Make sure they exist and your bucket is in the same region as this function.`;
+		console.log(message);
+		throw new Error(message);
+	}
 };
